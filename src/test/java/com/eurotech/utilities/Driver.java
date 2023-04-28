93f15dbec33387b26328796cf6a98f6d66751f0c
AliSR872 <ali.serdan89@gmail.com>
2023-04-28 15:44:49 +0300

    As

--- src/test/java/com/eurotech/utilities/Driver.java
+++ src/test/java/com/eurotech/utilities/Driver.java
@@ -1,6 +1,6 @@
 package com.eurotech.utilities;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
@@ -34,19 +34,19 @@ public class Driver {
             String browser = System.getProperty("browser") != null ? browser = System.getProperty("browser") : ConfigurationReader.get("browser");
             switch (browser) {
                 case "chrome":
-                    WebDriverManager.chromedriver().setup();
+
                     driverPool.set(new ChromeDriver());
                     break;
                 case "chrome-headless":
-                    WebDriverManager.chromedriver().setup();
+
                     driverPool.set(new ChromeDriver(new ChromeOptions().setHeadless(true)));
                     break;
                 case "firefox":
-                    WebDriverManager.firefoxdriver().setup();
+
                     driverPool.set(new FirefoxDriver());
                     break;
                 case "firefox-headless":
-                    WebDriverManager.firefoxdriver().setup();
+
                     driverPool.set(new FirefoxDriver(new FirefoxOptions().setHeadless(true)));
                     break;
                 case "ie":
