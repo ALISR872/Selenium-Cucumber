1e5d6cadecd4e0b97b45205c17bf13a6c2f7a3a9
AliSR872 <ali.serdan89@gmail.com>
2023-04-28 15:47:07 +0300

    As2.2

--- src/test/java/com/eurotech/utilities/Driver.java
+++ src/test/java/com/eurotech/utilities/Driver.java
@@ -2,7 +2,7 @@ package com.eurotech.utilities;
 
 
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
